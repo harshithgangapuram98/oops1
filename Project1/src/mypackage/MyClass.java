f05d4ca65aa3de8fa49a9c2163a4f41d3ff56d95
harshith <harshithgangapuram98@gmail.com>
2022-07-20 09:50:09 +0530

    oopsconcepts

---
+++ Project1/src/mypackage/MyClass.java
@@ -0,0 +1,17 @@
+package mypackage;
+
+public class MyClass {
+
+	public static void main(String[] args) 
+	
+	{
+		
+		
+		Employee emp = new Employee(101,"Harshith",3000);
+		
+		
+		emp.printData();
+		
+	}
+
+}
